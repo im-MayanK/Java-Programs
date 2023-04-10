47077ce6ab4f6e0d8a727e1900f06c4539720fe1
im-MayanK <mayankagrawal0807@gmail.com>
2023-04-10 17:03:23 +0530

    hello java and typecasting

---
+++ typecasting.java
@@ -0,0 +1,14 @@
+public class typecasting {
+    public static void main(String[] args) {
+        int a=45;
+        double b;
+      b=a;
+      System.out.println(b);//implicit typecasting
+      double num1 =45.5;
+      int num2;
+      num2= (int) num1;
+      System.out.println(num2);//explicit typecasting
+
+    }
+    
+}
